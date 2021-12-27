38b639620fe6fbf692c730619c088fd12448117e
Mohan091 <76912638+Mohan091@users.noreply.github.com>
2021-12-27 17:12:53 +0530

    Update Install_dependencies.sh

--- Scripts/Install_dependencies.sh
+++ Scripts/Install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo apt update -y
+curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt update -y 
 sudo npm install
+sudo npm install -g npm@latest
