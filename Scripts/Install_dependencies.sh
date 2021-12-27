8bf22c9492c26eabab4afeb2d98ce8892fdbffa7
Mohan091 <76912638+Mohan091@users.noreply.github.com>
2021-12-27 17:46:33 +0530

    Delete Scripts directory

--- Scripts/Install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt update -y 
-sudo npm install
-sudo npm install -g npm@latest
