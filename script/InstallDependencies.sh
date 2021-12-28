8b06341cfa84224628d0390f182c0e3e41ba0609
Mohan091 <76912638+Mohan091@users.noreply.github.com>
2021-12-28 11:57:14 +0530

    Update InstallDependencies.sh

--- script/InstallDependencies.sh
+++ script/InstallDependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt update -y
-curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
-apt-get install -y nodejs
+sudo apt-get install -y nodejs
 sudo npm install -g npm@latest
 sudo npm install
 sudo npm install pm2 -g
